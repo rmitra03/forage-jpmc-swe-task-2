2924442370e5e56de2c8964c70454cac80d12ff5
rmitra03 <risheeka.mitra@gmail.com>
2023-12-13 10:16:23 -0600

    Made changes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  // Added new property "showGraph" of type boolean
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // Initialized new property "showGraph"
+      showGraph: false,
     };
   }
 
@@ -29,18 +33,29 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    // User must click "Start Streaming", so if statement determines whether it's been clicked or not
+    if(this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x = 0; // Serves for continuous data collection from server
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({ // Uses interface variables
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) { // When x reaches the guard value for timeout
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
